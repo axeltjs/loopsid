ed5e1bf0e62965da55f2926221bfea3aed525384
axeltjs <axel@thortech.asia>
2020-03-28 23:24:40 +0800

    A: Laravel, Theme, Models, and Migrations

---
+++ database/migrations/2020_03_28_151917_comments.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('website')->nullable();
+            $table->text('comment');
+            $table->timestamps();
+
+            $table->foreign('post_id')
+                    ->references('id')
+                    ->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
